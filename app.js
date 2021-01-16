85a1c8659067f2cc0d0d684f9b74ab22168ecf3f
XuTongle <xutongle@gmail.com>
2021-01-17 03:39:25 +0800

    git

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 import config from './config'
 
 App({
-  onLaunch() {
+  globalData: {
+    appName: config.appName,
+        systemInfo: {},
+        userInfo: null
+  },
+  onLaunch(options) {
+    console.log('生命周期函数--监听小程序初始化' + JSON.stringify(options))
+    this.systemInfo();
+
+    // 小程序主动更新
+    this.updateManager();
+    console.log('App名称：' + this.globalData.appName)
+    console.log('系统参数：' + JSON.stringify(this.globalData.systemInfo))
+    
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -34,7 +47,53 @@ App({
       }
     })
   },
-  globalData: {
-    userInfo: null
+  onShow: function () {
+    console.log('生命周期函数--监听小程序显示')
+},
+onHide: function () {
+    console.log('生命周期函数--监听小程序隐藏')
+},
+updateManager: function () {
+  // 获取小程序更新机制兼容
+  if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+          // 请求完新版本信息的回调
+          if (res.hasUpdate) {
+              updateManager.onUpdateReady(function () {
+                  wx.showModal({
+                      title: '更新提示',
+                      content: '新版本已经准备好，是否重启应用？',
+                      success: function (res) {
+                          if (res.confirm) {
+                              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                              updateManager.applyUpdate()
+                          }
+                      }
+                  })
+              })
+              updateManager.onUpdateFailed(function () {
+                  // 新的版本下载失败
+                  wx.showModal({
+                      title: '已经有新版本了哟~',
+                      content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
+                  })
+              })
+          }
+      })
+  } else {
+      // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+      wx.showModal({
+          title: '提示',
+          content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
   }
+},
+systemInfo: function () {
+  wx.getSystemInfo({
+      success: res => {
+          this.globalData.systemInfo = res;
+      }
+  });
+}
 })
